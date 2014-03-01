b60aefdac63d8df16df1c7d7c9e61014cb33f09d
Chris Carr <chris@v1020-wn-61-236.campus-dynamic.uwaterloo.ca>
2014-02-28 20:53:06 -0500

    Suggestions

--- prevent.php
+++ prevent.php
@@ -9,8 +9,25 @@ $roomCount = count($loadedFiles[3]) - 1;
 $percentAgeTotal = $loadedFiles[3][$roomCount][$age];
 $percentOfAge = $percentOfTotal * (100 / $percentAgeTotal);
 echo('<div>Percent of Unintentional Injuries in ' . $loadedFiles[3][$room][0] . ' ' . $percentOfAge . '%</div>');
+$prevention = array();
+$stairs = array();
+$stairs[] = 'A handrail, or one on each side of the stairs';
+$stairs[] = 'A properly installed carpet or traction mat on each step';
+$stairs[] = 'Gates at the top and bottom of the stairwell';
+$stairs[] = 'Well lit stairwell';
+$stairs[] = 'Keep the stairwell and landing free from objects to step on';
+$kitchen = array();
+$kitchen[] = 'Turn your pot handles in';
+$kitchen[] = 'Get safety locks for your cub board doors/drawers';
+$kitchen[] = 'Gate off the area';
+$prevention['KITCHEN'] = $kitchen;
 if($percentOfAge > 5){
 	echo('<div>Injuries in '.$loadedFiles[3][$room][0].' are very common for this age group. We suggest investing in some preventative actions.</div>');
+	if(isset($prevention[$loadedFiles[3][$room][0]])){
+		foreach($prevention[$loadedFiles[3][$room][0]] as $suggestion){
+			echo('<div>'.$suggestion.'</div>');
+		}
+	}
 }
 ?>
  
