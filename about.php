f377dc4254e70f8eb8dbf510cf00c1fb3e3e7692
Chris Carr <chris@v1020-wn-61-236.campus-dynamic.uwaterloo.ca>
2014-03-01 17:11:06 -0500

    Squalid

--- about.php
+++ about.php
@@ -3,11 +3,15 @@
 <html>
 <body>
 <p>Hackathon: CODE <a href="https://www.canadianopendataexperience.com/">https://www.canadianopendataexperience.com/</a></p>
+<p>Theme: Solving Problems and Increasing Productivity with <a href="http://data.gc.ca/">Open Data</a></p>
+<p>Solution: The goal of the app is to reduce unintentional injuries with information for parents and guardians. For every injury reduced is one less trip to the hospital.</p>
 <p>Team: Demo First - Chris Carr</p>
 <p>App: Squalid Salad</p>
 <p>Dataset: Unintentional injuries in Canada 2010</p>
 <p>Canadian Hospitals Injury Reporting and Prevention Program (CHIRPP) is a computerized information system that collects and analyzes data on injuries to people (mainly children) who are seen at the emergency rooms of the 10 pediatric hospitals and of 4 general hospitals in Canada. CHIRPP is a unique, richly detailed database of "pre-event" injury information obtained by asking:What was the injured person doing when the injury happened?, What went wrong? and Where did the injury occur?</p>
 <p><a href="http://data.gc.ca/data/en/dataset/3a78ceb8-fd71-40dd-9f42-1c1af158bf0f">http://data.gc.ca/data/en/dataset/3a78ceb8-fd71-40dd-9f42-1c1af158bf0f</a></p>
+<p>I also used child safety information from <a href="http://www.childsafetylink.ca/">http://www.childsafetylink.ca/</a></p>
+<p>Limitations: The age range was not fined grained enough to give different percentage of injuries for each age group listed. The raw data would have been helpful as well to sort the advice. I can not say which type of injury is most likely to occur in which room with the data I have now.</p>
 <p>Hosting: <a href="http://digitalocean.com">digitalocean.com</a></p>
 <p>url: <a href="http://107.170.48.43/squalidsalad/">http://107.170.48.43/squalidsalad/</a></p>
 <p><a href="mailto:ccarrster@gmail.com">ccarrster@gmail.com</a></p>
